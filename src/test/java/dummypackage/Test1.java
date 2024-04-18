70b22f68e9e1b80e2d781b1dda2ab06b53827323
veeresh1994 <bv188558@gmaik.com>
2024-04-19 02:24:54 +0530

    Adding Test1 to the master repo

---
+++ src/test/java/dummypackage/Test1.java
@@ -0,0 +1,20 @@
+package dummypackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://makemytrip.com");
+        
+		
+		
+	}
+
+}
+
